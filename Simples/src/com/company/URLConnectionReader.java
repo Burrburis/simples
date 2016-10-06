fd7a8e039d7eed5381d8a6057ee436a8e4c17751
Alex Zhukov <Burrburis@yandex.ru>
2016-10-06 16:21:31 +0300

    Work

--- Simples/src/com/company/URLConnectionReader.java
+++ Simples/src/com/company/URLConnectionReader.java
@@ -19,33 +19,46 @@ class BufferReaderFile {
         }
         bufferedReader.close();
     }
+
+    public BufferReaderFile(Reader potok) throws Exception  {
+        BufferedReader bufferedReader = new BufferedReader(potok);
+        file = "";
+        while ((Line = bufferedReader.readLine()) != null) {
+            file = file + Line;
+        }
+        bufferedReader.close();
+    }
 }
 
 public class URLConnectionReader {
     private static String url;
-
     public static void main(String[] args) throws Exception {
         url = "https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?match_id=2684466831&key=4CF8B3DF4EF7B7BBD041E21E620DB912";
         URL oracle = new URL(url);
         URLConnection yc = oracle.openConnection();
+        BufferReaderFile readerFile = new BufferReaderFile("data/abilities.json");
+        BufferReaderFile readerpotok = new BufferReaderFile(new InputStreamReader(yc.getInputStream()));
+        System.out.println(readerFile.file);
+        System.out.println(readerpotok.file);
+    }
+}
 
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(
+      /*  BufferedReader in = new BufferedReader(new InputStreamReader(
                 yc.getInputStream()));
+        BufferedReader s = new BufferedReader(new FileReader("1"));
         String inputLine;
         String Json = "";
         while ((inputLine = in.readLine()) != null) {
             Json = Json + inputLine;
         }
         in.close();
+
         JSONObject jsonObject = new JSONObject(Json);
         JSONObject match = jsonObject.getJSONObject("result");
         String matchresult = jsonObject.getString("result");
         JSONArray players = match.getJSONArray("players");
         JSONObject player = players.getJSONObject(1);
         String idplayer = player.getString("account_id");
-
-        BufferReaderFile readerFile = new BufferReaderFile("data/abilities.json");
         System.out.println(readerFile.file);
         String abilities = readerFile.file;
         JSONObject jsonAbilities = new JSONObject(abilities);
@@ -53,6 +66,4 @@ public class URLConnectionReader {
         JSONObject abilitie = arrayAbilities.getJSONObject(0);
         String nameabilitie = abilitie.getString("name");
         System.out.println(nameabilitie);
-    }
-}
-
+        */
