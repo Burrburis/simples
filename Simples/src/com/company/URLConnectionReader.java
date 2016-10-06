084bfc337f1a09da5873174fdeb26fdef5a7a657
Alex Zhukov <Burrburis@yandex.ru>
2016-10-06 15:13:04 +0300

    Work

--- Simples/src/com/company/URLConnectionReader.java
+++ Simples/src/com/company/URLConnectionReader.java
@@ -1,12 +1,25 @@
 import org.json.JSONArray;
 import org.json.JSONObject;
-
+import java.net.*;
+import java.io.*;
 //API Site
 //https://developer.valvesoftware.com/wiki/Steam_Web_API#GetGlobalStatsForGame_.28v0001.29
 
-import java.net.*;
-import java.io.*;
 
+
+class BufferReaderFile {
+ private String Line;
+         String file;
+
+    public BufferReaderFile(String filepath) throws Exception  {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(filepath));
+        file = "";
+        while ((Line = bufferedReader.readLine()) != null) {
+            file = file + Line;
+        }
+        bufferedReader.close();
+    }
+}
 
 public class URLConnectionReader {
     private static String url;
@@ -15,6 +28,8 @@ public class URLConnectionReader {
         url = "https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?match_id=2684466831&key=4CF8B3DF4EF7B7BBD041E21E620DB912";
         URL oracle = new URL(url);
         URLConnection yc = oracle.openConnection();
+
+
         BufferedReader in = new BufferedReader(new InputStreamReader(
                 yc.getInputStream()));
         String inputLine;
@@ -23,29 +38,21 @@ public class URLConnectionReader {
             Json = Json + inputLine;
         }
         in.close();
-        data/abilities.json
         JSONObject jsonObject = new JSONObject(Json);
         JSONObject match = jsonObject.getJSONObject("result");
-        //String matchresult = jsonObject.getString("result");
+        String matchresult = jsonObject.getString("result");
         JSONArray players = match.getJSONArray("players");
         JSONObject player = players.getJSONObject(1);
-        String name = player.getString("account_id");
+        String idplayer = player.getString("account_id");
 
-        // System.out.println(match);
-        System.out.println(players.length());
-        System.out.println(name);
-        System.out.println(matchresult);
+        BufferReaderFile readerFile = new BufferReaderFile("data/abilities.json");
+        System.out.println(readerFile.file);
+        String abilities = readerFile.file;
+        JSONObject jsonAbilities = new JSONObject(abilities);
+        JSONArray arrayAbilities = jsonAbilities.getJSONArray("abilities");
+        JSONObject abilitie = arrayAbilities.getJSONObject(0);
+        String nameabilitie = abilitie.getString("name");
+        System.out.println(nameabilitie);
     }
 }
 
-    File f = new File("data/1.txt");
-    FileInputStream fop = null;
-        try {
-                fop = new FileInputStream(f);
-                } catch (FileNotFoundException e) {
-                e.printStackTrace();
-                }
-                int i = -1;
-                while ((i = fop.read() )!= -1) {
-                System.out.println((char)i);
-                }
